fba6cac5ce8a6db1dba8af0875b16c4ad878d340
SkyWalker De Schrödinger <paparacing165@gmail.com>
2025-08-14 14:04:22 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+// URL Thumbnail Global untuk Menu
+global.thumb = 'https://cdn.ypnk.biz.id/yp/xzyy1549.jpg';
+// url telegram owner nya
+global.tme = 'https://t.me/skyvoidwalker'
+// Token Bot dari @BotFather
+global.bottoken = '-';
+
+// Nama Bot dll
+global.botname = 'Columbina';
+global.botVersion = '1.0.0';
+global.botMode = 'public';
+global.dfail = (type, m) => {
+    const messages = {
+        owner: '*Perintah Owner*\nFitur ini hanya dapat diakses oleh Owner Bot.',
+        group: '*Khusus Grup*\nPerintah ini hanya dapat digunakan di dalam grup.',
+        private: '*Khusus Private Chat*\nPerintah ini hanya dapat digunakan di private chat.',
+        admin: '*Perintah Admin*\nFitur ini hanya dapat diakses oleh Admin Grup.',
+        botAdmin: '*Bot Bukan Admin*\nJadikan bot sebagai admin untuk menggunakan perintah ini.',
+        limit: '*Limit Habis*\nLimit harian Anda telah habis. Coba lagi besok.',
+    };
+    const message = messages[type];
+    if (message) {
+        m.reply(message);
+    }
+};
+
+// Prefix Perintah
+global.prefix = '/';
+
+// Informasi Owner
+global.owner = {
+    name: 'SkyWalker',
+    // nama dan ID Telegram owner
+    id: 6861815787 
+};
